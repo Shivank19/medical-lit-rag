05c54f41f5d74ffa1c5a6415a051a3410fcf8359
Shivank19 <shivank.sapra@gmail.com>
2026-05-28 01:37:37 -0500

    update app root

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-from app.ui import demo
+from pathlib import Path
+import sys
+
+APP_DIR = Path(__file__).resolve().parent / "app"
+sys.path.insert(0, str(APP_DIR))
+
+from ui import demo
 
 demo.launch()
